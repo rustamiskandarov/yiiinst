eab41d81aaf258c53bf5875e7a87d19959291419
Rust <admin@helper.com>
2018-03-29 01:36:30 +0300

    start project

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -43,6 +43,23 @@ return [
             'rules' => [
             ],
         ],
+        'authClientCollection' => [
+            'class' => 'yii\authclient\Collection',
+            'clients' => [
+                'google' => [
+                    'class' => 'yii\authclient\clients\Google',
+                    'clientId' => 'google_client_id',
+                    'clientSecret' => 'google_client_secret',
+                ],
+                'facebook' => [
+                    'class' => 'yii\authclient\clients\Facebook',
+                    'clientId' => 'facebook_client_id',
+                    'clientSecret' => 'facebook_client_secret',
+                ],
+                // etc.
+            ],
+        ]
+
     ],
     'params' => $params,
 ];
